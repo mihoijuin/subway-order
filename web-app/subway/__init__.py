e4f106e04f8e521598294477d78f94f27268c5c9
Miho Ijuin <miiiiiiiii747@gmail.com>
2018-08-30 16:58:11 +0900

    [add]refs #32 トップページを読み込むスクリプトを追加(処理なし）

--- web-app/subway/__init__.py
+++ web-app/subway/__init__.py
@@ -5,6 +5,3 @@ app = Flask(__name__)
 @app.route('/')
 def show_toppage():
     return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True)
