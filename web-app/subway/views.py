b3594c4a311de8ff7c69e8f6666713a977cff998
Miho Ijuin <miiiiiiiii747@gmail.com>
2018-09-05 23:30:54 +0900

    [update]refs #58 各フォームの値を変数に格納
    トッピング、野菜量、ドレッシングは複数なのでリストに格納

--- web-app/subway/views.py
+++ web-app/subway/views.py
@@ -12,13 +12,22 @@ def toppage():
         form=form
     )
 
-@app.route('/result', methods = ['GET', 'POST'])
+@app.route('/result', methods = ['POST'])
 def result():
     title='Play Sandwich'
-    if request.method == 'POST':
-        result = request.form
+    vege_list = ['lettus', 'tomato', 'green', 'onion', 'carrot', 'olive', 'pickles', 'hot']
+    if request.form:
+        user_sand = request.form['sand']
+        user_bread = request.form['bread']
+        user_toppings = request.form.getlist('topping')
+        vege_amounts = [ request.form[i] for i in vege_list]
+        user_souces = request.form.getlist('source')
         return render_template(
             'result.html',
             title=title,
-            result=result
+            user_sand=user_sand,
+            user_bread=user_bread,
+            user_toppings=user_toppings,
+            vege_amounts=vege_amounts,
+            user_souce=user_souces
             )
