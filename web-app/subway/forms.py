f912f135c35dd99a8ffcaade7f8f5213951d70d3
Miho Ijuin <miiiiiiiii747@gmail.com>
2018-09-05 14:30:09 +0900

    [add]refs #58 サンド選択用ラジオフォームをWTFormsで記述

--- web-app/subway/forms.py
+++ web-app/subway/forms.py
@@ -1,1 +1,23 @@
 # メニューを選択するためのフォームを作成するクラス
+from wtforms import RadioField, SubmitField, SelectMultipleField
+from wtforms.validators import Required
+from flask_wtf import Form
+
+class SandwichForms(Form):
+    sand = RadioField('サンドイッチを選択してね', [Required(message='どれか一つ選んでね')], choices=[
+        ('beaf', 'ローストビーフ'),
+        ('rawham', '生ハムマスカルポーネ'),
+        ('shrimp', 'えびアボカド'),
+        ('teriyaki', '照り焼きチキン'),
+        ('blt', 'BLT'),
+        ('chicken', 'ローストチキン'),
+        ('turkey_egg', 'ターキーベーコンエッグ'),
+        ('cheese_chicken', 'チーズローストチキン'),
+        ('turkey', 'ターキーブレスト'),
+        ('tuna', 'ツナ'),
+        ('chile', 'チリチキン'),
+        ('avocado', 'アボカドベジー'),
+        ('egg', 'たまご'),
+        ('vege_cheese', 'べジーチーズ'),
+        ('vege', 'ベジーデライト')
+    ])
