ed05803f69ecd187edd868e69272fcecadb13457
Miho Ijuin <miiiiiiiii747@gmail.com>
2018-09-05 16:09:29 +0900

    [add]refs #59 トッピング選択用の複数チェックボックスフォームを作成
    作成したMultiCheckboxFieldを使用

--- web-app/subway/forms.py
+++ web-app/subway/forms.py
@@ -9,7 +9,7 @@ class MultiCheckboxField(SelectMultipleField):
     option_widget = CheckboxInput()
 
 class SandwichForms(Form):
-    sand = RadioField('サンドイッチを選択してね', [Required(message='サンドイッチはどれか一つを選んでね')], choices=[
+    sand = RadioField('サンドイッチを選ぼう', [Required(message='サンドイッチはどれか一つを選んでね')], choices=[
         ('beaf', 'ローストビーフ'),
         ('rawham', '生ハムマスカルポーネ'),
         ('shrimp', 'えびアボカド'),
@@ -26,10 +26,21 @@ class SandwichForms(Form):
         ('vege_cheese', 'べジーチーズ'),
         ('vege', 'ベジーデライト')
     ])
-    bread = RadioField('パンを選択してね', [Required(message='パンはどれか一つを選んでね')], choices=[
+    bread = RadioField('パンを選ぼう', [Required(message='パンはどれか一つを選んでね')], choices=[
         ('honey', 'ハニーオーツ'),
         ('wheet', 'ウィート'),
         ('sesame', 'セサミ'),
         ('white', 'ホワイト'),
         ('flat', 'フラットブレッド')
+    ])
+    topping = MultiCheckboxField('【有料】トッピングを選ぼう', [Required(message='トッピングをつけたくない場合は無しを選択してね')], choices=[
+        ('none', 'なし'),
+        ('cheese', 'スライスチーズ'),
+        ('cream', 'クリームタイプチーズ'),
+        ('mascarpone', 'マスカルポーネチーズ'),
+        ('egg', 'たまご'),
+        ('bacon', 'ベーコン'),
+        ('tuna', 'ツナ'),
+        ('shrimp', 'えび'),
+        ('avocado', 'アボカド')
     ])
