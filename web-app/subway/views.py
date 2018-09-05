2ba10343403f5427793cf78483388a17e7e96620
Miho Ijuin <miiiiiiiii747@gmail.com>
2018-09-05 15:18:30 +0900

    [add]refs #61 フォームを読み込みテンプレートにrenderする
    フォームはforms.pyに記述

--- web-app/subway/views.py
+++ web-app/subway/views.py
@@ -1,13 +1,15 @@
-from flask import render_template
+from flask import render_template, request
 from subway import app
-
+from subway.forms import SandwichForms
 
 @app.route('/')
 def toppage():
+    form = SandwichForms(request.form)
     title='Play Sandwich'
     return render_template(
         'index.html',
-        title=title
+        title=title,
+        form=form
     )
 
 
